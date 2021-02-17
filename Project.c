030843209b94e7ffae973e81839a4de7ea9fc55e
sathvik-bhat <75574704+sathvik-bhat@users.noreply.github.com>
2021-02-17 09:50:52 +0530

    Add files via upload

--- Project.c
+++ Project.c
@@ -7,7 +7,6 @@ struct Bitmap_Header
     int reserved;
     int data_offset; 
 };
-
 struct Info_Header
 {
     unsigned int size;
@@ -15,59 +14,94 @@ struct Info_Header
     unsigned int height;
     short int planes;
     short int bits_per_pixel;
-    // int compression;
     unsigned int compression;
+    unsigned int image_size;
     unsigned int X_pixels_per_m;
     unsigned int Y_pixels_per_m;
     int colours_used;
     int important_colours;
 };
 
-
-void read()
+void read_source()
 {
-    FILE *fp = fopen("sample.bmp","rb");
+    FILE *fp = fopen("sample.bmp", "rb");
+
     struct Bitmap_Header header;
     struct Info_Header h;
-
-    fread(header.signature,2,1,fp);
-    fread(&header.file_size,4,1,fp);
-    fread(&header.reserved,4,1,fp);
-    fread(&h,18,1,fp);
-    fread(&h.bits_per_pixel,2,1,fp);
-
-
-    printf("%c%c %u %d %d", header.signature[0],header.signature[1],header.file_size,header.reserved,h.bits_per_pixel);
-}
-
-void write()
-{
-    FILE *fpf = fopen("final.bmp","wb");
-    // struct Bitmap_Header header;
-    
-
-    FILE *fp = fopen("screenshot.bmp","rb");
-    struct Bitmap_Header header;
     fread(header.signature,2,1,fp);
     fread(&header.file_size,4,1,fp);
     fread(&header.reserved,4,1,fp);
     fread(&header.data_offset,4,1,fp);
+    fread(&h.size, 4, 1, fp);
+    fread(&h.width, 4, 1,fp);
+    fread(&h.height, 4, 1,fp);
+    fread(&h.planes, 2, 1,fp);
+    fread(&h.bits_per_pixel,2,1,fp);
+    fread(&h.compression, 4, 1,fp);
+    fread(&h.image_size, 4, 1,fp);
+    fread(&h.X_pixels_per_m, 4, 1,fp);
+    fread(&h.Y_pixels_per_m, 4, 1,fp);
+    fread(&h.colours_used, 4, 1,fp);
+    fread(&h.important_colours, 4, 1,fp);
 
-    printf("%c%c %u %d %d\n", header.signature[0],header.signature[1],header.file_size,header.reserved,header.data_offset);
+    fclose(fp);
+}
 
+const int bmpfileheaderSize = 14;
+const int bmpinfoheaderSize = 40;
 
+void create_imageheader(struct Bitmap_Header header, struct Info_Header h)
+{
+	// unsigned char bmpfileheader[14] = {
+	// 				'B', 'M',	// signature
+	//        				0, 0, 0, 0, 	// file size
+	// 				0, 0, 0, 0, 	// reserved
+	// 				54, 0, 0, 0	// data offset
+	// 				};
+	
+	// unsigned char bmpinfoheader[40] = {
+	// 				40, 0, 0, 0,	// info header size
+	// 				0, 0, 0, 0,	// width	
+	// 				0, 0, 0, 0,	// height
+	// 				1, 0,	// number colour planes
+	// 				24, 0,	// bits per pixel
+	// 				0, 0, 0, 0,	// compression is none
+	// 				0, 0, 0, 0,	// image bits size
+	// 				0x13, 0x0B, 0, 0,	// horizontal resolution in pixel
+	// 				0x13, 0x0B, 0, 0,	// vertical resolution in pixel
+	// 				0, 0, 0, 0,	// colours used
+	// 				0, 0, 0, 0,	// important colours
+	// 				};
+
+	FILE *fpf = fopen("output.bmp", "wb");
+    int a=24;
     fwrite(header.signature,2,1,fpf);
     fwrite(&header.file_size,4,1,fpf);
     fwrite(&header.reserved,4,1,fpf);
-    
     fwrite(&header.data_offset,4,1,fpf);
-}
+    fwrite(&h.size, 4, 1, fpf);
+    fwrite(&h.width, 4, 1,fpf);
+    fwrite(&h.height, 4, 1,fpf);
+    fwrite(&h.planes, 2, 1,fpf);
+    fwrite(&a,2,1,fpf);
+    fwrite(&h.compression, 4, 1,fpf);
+    fwrite(&h.image_size, 4, 1,fpf);
+    fwrite(&h.X_pixels_per_m, 4, 1,fpf);
+    fwrite(&h.Y_pixels_per_m, 4, 1,fpf);
+    fwrite(&h.colours_used, 4, 1,fpf);
+    fwrite(&h.important_colours, 4, 1,fpf);
 
+
+	// fwrite(bmpfileheader, 1, bmpfileheaderSize, fpf);
+	// fwrite(bmpinfoheader, 1, bmpinfoheaderSize, fpf);
+
+	fclose(fpf);
+}
 
 int main()
 {
-    // write();
-    read();
-    // printf("%ld",sizeof(short int));
-}
+    struct Bitmap_Header header; struct Info_Header h;
+    read_source();
+    create_imageheader(header, h);
 
+}
