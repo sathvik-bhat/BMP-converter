197c1c1c84204c4987e95c808d98baacc42c7d3b
Surya Sastry <78991131+Surya-Sastry@users.noreply.github.com>
2021-02-23 08:35:16 +0530

    Add files via upload

---
+++ dec.h
@@ -0,0 +1,7 @@
+void read_source(FILE*);
+void Image_pixel(FILE *fp, int height, int width);
+void Gray_to_rgb(int height,int width);
+void read_source(FILE* fp);
+void create_imageheader(FILE *fpf);
+void Copy_pixels_to_destination(FILE *fpf);
+void input_output(FILE *fp);
