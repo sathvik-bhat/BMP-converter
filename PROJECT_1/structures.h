54e666e56f2fdb5f35edc136cb0fcc682c992bfb
02prem <79003626+02prem@users.noreply.github.com>
2021-02-24 16:25:11 +0530

    Update structures.h

--- PROJECT_1/structures.h
+++ PROJECT_1/structures.h
@@ -1,24 +1,27 @@
+/* Bitmap header which is of the size 14 bytes */
 struct Bitmap_Header
 {
-    char signature[2];
-    unsigned int file_size;
+    char signature[2];	// Magic identifier that specifies the file type i.e. BM for any bitmap file
+    unsigned int file_size;	// size of the file
     int reserved;
-    int data_offset; 
+    int data_offset; 	// specifies the offset in bytes from the bitmapfileheader to the bitmap bits
 };
 
+/* Bitmap Information header */
 struct Info_Header
 {
-    unsigned int size;
-    unsigned int width;
-    unsigned int height;
-    short int planes;
-    short int bits_per_pixel;
-    unsigned int compression;
-    unsigned int image_size;
-    unsigned int X_pixels_per_m;
-    unsigned int Y_pixels_per_m;
-    int colours_used;
-    int important_colours;
+    unsigned int size;	// size of the info header
+    unsigned int width;	// width of the pixels
+    unsigned int height;	// height of the pixels
+    short int planes;	// specifies the number of color planes, must be 1
+    short int bits_per_pixel;	// specifies the number of bit per pixel
+    unsigned int compression;	// spcifies the type of compression
+    unsigned int image_size;	// spcifies the type of compression
+    unsigned int X_pixels_per_m;	// number of pixels per meter in x axis
+    unsigned int Y_pixels_per_m;	// number of pixels per meter in y axis
+    int colours_used;	// number of colors used by the bitmap
+    int important_colours;	// number of colors that are important
+
 };
 
 struct Greyscale
